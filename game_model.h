431951865ee2b64681f5e184f3f989d34c3a9e70
noname <272292@student.pwr.edu.pl>
2023-03-29 17:05:07 +0200

    sklep+lvl

--- game_model.h
+++ game_model.h
@@ -15,8 +15,10 @@ public:
     int get_points();
     void add_points(int p);
     void reset_points();
+    //za ka¿dy level gracz dostaje +1pkt za klikniecie
     int get_level();
     void add_level();
+    //gracz moze dokupic mno¿niki pkt per klik w sklepie
     void mod_multiplier(int m);
     int get_multiplier();
     void shop(int price);
